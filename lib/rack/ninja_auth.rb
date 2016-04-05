b6402959600f444a8201af71d5c6f4b70338a98f
JP Hastings-Spital <jp@deliveroo.co.uk>
2016-04-05 15:24:41 +0100

    Force cookie path

--- lib/rack/ninja_auth.rb
+++ lib/rack/ninja_auth.rb
@@ -9,6 +9,7 @@ module Rack
     class Middleware < Sinatra::Base
       use Rack::Accept
       use Rack::Session::Redis,
+        path: '/',
         key: 'rack.ninja_auth',
         expire_after: 2592000,
         redis_server: ENV['NINJA_REDIS_URL'] || 'redis://127.0.0.1:6379/0/rack:ninja_auth'
@@ -17,10 +18,11 @@ module Rack
         provider :google_oauth2, ENV["NINJA_GOOGLE_CLIENT_ID"], ENV["NINJA_GOOGLE_CLIENT_SECRET"]
       end
 
-      def initialize(app, email_matcher: //, secured_routes: //, not_allowed_file: nil)
+      def initialize(app, email_matcher: //, secured_routes: //, not_allowed_file: nil, remember_original_path: true)
         $stderr.puts "Please set NINJA_GOOGLE_CLIENT_ID and NINJA_GOOGLE_CLIENT_SECRET to use NinjaAuth" unless ENV["NINJA_GOOGLE_CLIENT_ID"] && ENV["NINJA_GOOGLE_CLIENT_SECRET"]
         @main_app = app
         @email_matcher = email_matcher
+        @remember_original_path = remember_original_path
         @secured_route_matcher = secured_routes
         @not_allowed_file = not_allowed_file || ::File.expand_path('../../../views/401.html', __FILE__)
         super()
@@ -39,7 +41,8 @@ module Rack
       get '/auth/google_oauth2/callback' do
         if (request.env["omniauth.auth"].info.email.match(@email_matcher) rescue false)
           session[:user] = request.env["omniauth.auth"].info.email
-          redirect session[:redirect_to]
+          redirect_url = session[:redirect_to] if @remember_original_path
+          redirect redirect_url || '/'
         else
           redirect '/auth/failure'
         end
@@ -52,7 +55,7 @@ module Rack
       after do
         if !@hit_real_app && status == 404
           halt(403) unless env['rack-accept.request'].media_type?('text/html')
-          session[:redirect_to] = is_internal_request? ? '/' : env['REQUEST_URI']
+          session[:redirect_to] = env['REQUEST_URI'] if @remember_original_path && is_internal_request?
           redirect '/auth/google_oauth2'
         end
       end
